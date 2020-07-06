fb2b85464130214689d95408728804348988ce40
Datum <allen.james.west@gmail.com>
2020-07-06 20:48:28 +0000

    intial test of first name form completion

--- dostuff.py
+++ dostuff.py
@@ -1,5 +1,7 @@
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 driver = webdriver.Firefox()
 driver.get("https://www.onecountry.com/giveaways/give-million-cash-entry/")
@@ -8,5 +10,6 @@ elemNameOne = driver.find_element_by_name('first_name')
 elemNameOne.clear()
 elemNameOne.send_keys('James')
 elemNameOne.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
 
 
